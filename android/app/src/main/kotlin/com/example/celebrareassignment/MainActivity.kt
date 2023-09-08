ba83bd62955fa3bb2b2bc3019da1770978f88772
Sankalp <sankalpdhama11@gmail.com>
2023-09-08 18:29:36 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/celebrareassignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.celebrareassignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/AddScreen.dart
@@ -0,0 +1,158 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddScreen extends StatefulWidget {
+  const AddScreen({super.key});
+
+  @override
+  State<AddScreen> createState() => _AddScreenState();
+}
+
+class _AddScreenState extends State<AddScreen> {
+  File? image;
+  int frame = 1;
+  bool isFrame = false;
+  void pickImage(ImageSource source) async {
+    final ImagePicker picker = ImagePicker();
+// Pick an image.
+    final image = await picker.pickImage(source: source);
+    var file = await ImageCropper().cropImage(
+        sourcePath: image!.path,
+        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 0.85));
+    final imageTemoparary = File(file!.path);
+    setState(() {
+      this.image = imageTemoparary;
+    });
+  }
+
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: const Text('Choose Frame'),
+          actions: <Widget>[
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      child: const Text('Heart'),
+                      onPressed: () {
+                        setState(() {
+                          frame = 1;
+                          isFrame = true;
+                        });
+                      },
+                    ),
+                    TextButton(
+                      child: const Text('Square'),
+                      onPressed: () {
+                        setState(() {
+                          frame = 2;
+                          isFrame = true;
+                        });
+                      },
+                    ),
+                    TextButton(
+                      child: const Text('Circle'),
+                      onPressed: () {
+                        setState(() {
+                          frame = 3;
+                          isFrame = true;
+                        });
+                      },
+                    ),
+                    TextButton(
+                      child: const Text('Rectangle'),
+                      onPressed: () {
+                        setState(() {
+                          frame = 4;
+                          isFrame = true;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isFrame=false;
+                          });
+                        },
+                        child: Text('No Frame')),
+                    ElevatedButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text('Done')),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: InkWell(
+          onTap: () => SystemNavigator.pop(),
+          child: Icon(Icons.close),
+        ),
+        title: Text('Add An Image'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(),
+              child: TextButton(
+                onPressed: () => pickImage(ImageSource.gallery),
+                child: Text('Upload An Image'),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => _dialogBuilder(context),
+              child: Text('Change Frame'),
+            ),
+            if (image != null)
+              Container(
+                height: 400,
+                child: Stack(alignment: AlignmentDirectional.center, children: [
+                  Image.file(
+                    image!,
+                    height: 400,
+                    // width: double.infinity,
+                  ),
+                  if (isFrame == true)
+                    Image.asset(
+                      'assets/user_image_frame_$frame.png',
+                      color: Colors.white,
+                      colorBlendMode: BlendMode.xor,
+                    ),
+                ]),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// image != null
+// ? Image.file(
+// image!,
+// )
+// : FlutterLogo(),
